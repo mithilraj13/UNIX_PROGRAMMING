53e28984ec8a36bfcbad93bdf6106673ce2911ed
mithilraj13 <82027839+mithilraj13@users.noreply.github.com>
2022-01-06 13:10:43 +0530

    Delete USP_5B_B2-main directory

--- USP_5B_B2-main/factorial.sh
+++
@@ -1,9 +0,0 @@
-echo -n "Enter the number :"
-read num 
-res=1
-while [ $rnum -gt 1 ]
-do
-	res=`expr $res \* $num`
-	num=`expr $res - 1`
-done
-echo "The factorial is $res"
--- USP_5B_B2-main/tempConvert.sh
+++
@@ -1,8 +0,0 @@
-echo -n "Enter the temperature in Fahrenheit: "
-read far
-temp=`echo "$far - 32"|bc`
-const=`echo "scale=2;(5 / 9)"|bc`
-c=`echo "$const * $temp" |bc`
-echo "The temperature is celcuis is: $c"
-
-
